76a6cf6503df9b8a08a93bcaa36d6e755949a658
Comelpho <comelpho@gmail.com>
2024-04-03 18:49:10 +0300

    Шапка

--- components/Header.vue
+++ components/Header.vue
@@ -4,9 +4,11 @@
       <Container>
         <ContainerBlock>
           <div class="header__container">
-            <Button class="header__container__menu" mod="green" type="button"> Меню </Button>
-            <img src="assets/images/logo.png" alt="Санаторий ЛЕС" class="header__container__logo" />
-            <a href="#" class="header__container__phoneButton">+7 800 700-09-06</a>
+            <button class="header__container__menu">Меню</button>
+            <NuxtLink to="/index.vue" class="header__container__logoButton">
+              <img src="assets/images/logo.png" alt="Санаторий ЛЕС" class="header__container__logoButton__logo"
+            /></NuxtLink>
+            <a href="tel:+78007000906" class="header__container__phoneButton">+7 800 700-09-06</a>
           </div>
         </ContainerBlock>
       </Container>
@@ -18,5 +20,54 @@
   import Container from './Container.vue';
   import ContainerBlock from './ContainerBlock.vue';
   //   import IconComponent from './IconComponent.vue';
-  import Button from './Button.vue';
 </script>
+
+<style lang="scss" scoped>
+  header {
+    top: 0;
+    position: absolute;
+    width: 100%;
+    z-index: 5;
+    .header {
+      width: 100%;
+      &__container {
+        width: calc(100% - 80px);
+        height: 80px;
+        border-radius: 0px 0px 40px 40px;
+        background-color: $white;
+        display: flex;
+        align-items: center;
+        padding: 0 40px;
+        &__menu {
+          font-family: 'Manrope';
+          font-size: 25px;
+          font-weight: 400;
+          line-height: 42px;
+          text-transform: uppercase;
+          color: $black;
+        }
+
+        &__logoButton {
+          position: absolute;
+          left: 50%;
+          transform: translate(-50%, 0);
+          &__logo {
+            height: 40px;
+            object-fit: cover;
+          }
+        }
+
+        &__phoneButton {
+          margin: 0 0 0 auto;
+          text-decoration: none;
+          font-family: 'Manrope';
+          font-size: 25px;
+          font-weight: 400;
+          line-height: 42px;
+          color: $black;
+          text-transform: uppercase;
+        }
+      }
+    }
+  }
+</style>
